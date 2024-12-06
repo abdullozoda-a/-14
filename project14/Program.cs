d75ba8b6313281be186d6a79cd70a6a5e7e13577
abdullozoda_oo8 <abdullozodaanush8@gmail.com>
2024-12-06 19:09:45 +0500

    create

---
+++ project14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Console = System.Console;
+
+namespace project14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            double Summa = 0;
+            for (int i=1; i<=n; i++)
+            {
+                Summa += 1.0 / i;
+            }
+            Console.WriteLine(Summa);
+        }
+    }
+}
